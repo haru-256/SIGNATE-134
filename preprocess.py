15ca537f68942d406d56bc196c0a4773862424a6
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-08-29 20:23:42 -0700

    first commit

---
+++ preprocess.py
@@ -0,0 +1,14 @@
+import pathlib
+import chainer
+import chainercv
+import numpy as np
+
+
+def main():
+    path = pathlib.Path('data')
+    train_path = path / 'train'
+    test_path = path / 'test'
+
+
+if __name__ == "__main__":
+    main()
